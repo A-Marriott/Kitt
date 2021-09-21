a5bfc436d7c94097aaad6c55de59aae4b16fda28
Alex Marriott <marriottaog@gmail.com>
2021-09-21 18:46:17 +0100

    function implemented but needs serious refactoring

--- lib/get_price.rb
+++ lib/get_price.rb
@@ -1,29 +1,44 @@
 def get_price(duration_minutes)
+  total_price = 0
+  duration_left = duration_minutes
+
+  weeks_booked = duration_left / 10080
+  total_price += 105 * weeks_booked
+  duration_left -= 10080 * weeks_booked
+
+  days_booked = duration_left / 1440
+  if days_booked >= 2
+    total_price += 105
+    return total_price
+  end
+  total_price += 60 * days_booked
+  duration_left -= 1440 * days_booked
+
+  hours_booked = duration_left / 60
+  if hours_booked >= 3
+    total_price += 60
+    return total_price
+  end
+  total_price += 22 * hours_booked
+  duration_left -= 60 * hours_booked
+
+  minutes_booked = duration_left
+  if minutes_booked >= 12
+    total_price += 22
+    return total_price
+  end
+  total_price += 2 * minutes_booked
+
+  return total_price
+
+  p total_price
+  p duration_left
 end
 
-# duration provided in minutes
-# Series of decreasing modulos, single if statements
-# have a price variable that increments as we go through
-# have a duration variable that decreases as we go through
-# First modulo = week
-# increment price count by 105 * modulo
-# decrease duration count by 10080 * modulo
-# Second modulo = day
-# if modulo >= 2, give them the week price, therefore increase price count by 105 (week cost) and return total price (exit function)
-# else if modulo is 1/0,
-# increment price count by 60*modulo,
-# decrease duration count by 1440*modulo
-# Third modulo = hour
-# if modulo >= 3, give them day price, increase price count by 60 (day cost), return total price (exit function)
-# else if modulo is 0/1/2
-# increment price count by 22*modulo
-# decrease duration count by 60*modulo
-# Fourth modulo = minute
-# if modulo >= 12, give them hour price, increase price count by 22, return total price
-# else if modulo is up to 11,
-# increment price by 2*modulo
-# no smaller increments so simply return price without worrying about duration
 
+get_price(20161)
 
 
 # this will be able to be refactored by calling a single function that deals with each level of week/day payments etc. instead of repeating the specifics of each if
+# maybe store information in an object for week price, week duration
+# use tarrif as term
