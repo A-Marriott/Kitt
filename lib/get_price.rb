c1a3efa73d460c2e667b78c68506ac09c762772f
Alex Marriott <marriottaog@gmail.com>
2021-09-23 20:20:59 +0100

    added hash for minutes conversion reference

--- lib/get_price.rb
+++ lib/get_price.rb
@@ -6,28 +6,34 @@ def get_price(duration_minutes)
     minute: 2
   }
 
+  minutes_conversion = {
+    week: 10080,
+    day: 1440,
+    hour: 60
+  }
+
   total_price = 0
   duration_left = duration_minutes
 
-  weeks_booked = duration_left / 10080
+  weeks_booked = duration_left / minutes_conversion[:week]
   total_price += tariff[:week] * weeks_booked
-  duration_left -= 10080 * weeks_booked
+  duration_left -= minutes_conversion[:week] * weeks_booked
 
-  days_booked = duration_left / 1440
+  days_booked = duration_left / minutes_conversion[:day]
   if days_booked >= 2
     total_price += tariff[:week]
     return total_price
   end
   total_price += tariff[:day] * days_booked
-  duration_left -= 1440 * days_booked
+  duration_left -= minutes_conversion[:day] * days_booked
 
-  hours_booked = duration_left / 60
+  hours_booked = duration_left / minutes_conversion[:hour]
   if hours_booked >= 3
     total_price += tariff[:day]
     return total_price
   end
   total_price += tariff[:hour] * hours_booked
-  duration_left -= 60 * hours_booked
+  duration_left -= minutes_conversion[:hour] * hours_booked
 
   minutes_booked = duration_left
   if minutes_booked >= 12
