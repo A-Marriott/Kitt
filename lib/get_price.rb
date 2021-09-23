bfd75f0db8988a2e9ce08d745a2e3c399d5fb368
Alex Marriott <marriottaog@gmail.com>
2021-09-23 20:18:17 +0100

    added hash for tariff reference

--- lib/get_price.rb
+++ lib/get_price.rb
@@ -1,42 +1,45 @@
 def get_price(duration_minutes)
+  tariff = {
+    week: 105,
+    day: 60,
+    hour: 22,
+    minute: 2
+  }
+
   total_price = 0
   duration_left = duration_minutes
 
   weeks_booked = duration_left / 10080
-  total_price += 105 * weeks_booked
+  total_price += tariff[:week] * weeks_booked
   duration_left -= 10080 * weeks_booked
 
   days_booked = duration_left / 1440
   if days_booked >= 2
-    total_price += 105
+    total_price += tariff[:week]
     return total_price
   end
-  total_price += 60 * days_booked
+  total_price += tariff[:day] * days_booked
   duration_left -= 1440 * days_booked
 
   hours_booked = duration_left / 60
   if hours_booked >= 3
-    total_price += 60
+    total_price += tariff[:day]
     return total_price
   end
-  total_price += 22 * hours_booked
+  total_price += tariff[:hour] * hours_booked
   duration_left -= 60 * hours_booked
 
   minutes_booked = duration_left
   if minutes_booked >= 12
-    total_price += 22
+    total_price += tariff[:hour]
     return total_price
   end
-  total_price += 2 * minutes_booked
+  total_price += tariff[:minute] * minutes_booked
 
   return total_price
-
-  p total_price
-  p duration_left
 end
 
-
-get_price(20161)
+p get_price(2880)
 
 
 # this will be able to be refactored by calling a single function that deals with each level of week/day payments etc. instead of repeating the specifics of each if
