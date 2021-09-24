4bd4319a24fbd6bba023e15afe01ac431917201f
Alex Marriott <marriottaog@gmail.com>
2021-09-24 19:37:51 +0100

    change to comments

--- lib/get_price.rb
+++ lib/get_price.rb
@@ -1,4 +1,3 @@
-# Unit referes to unit of time, e.g. hour
 def get_price(unit, duration_minutes)
   info = if next_unit_of_time[unit]
            get_price(next_unit_of_time[unit], duration_minutes)
@@ -10,15 +9,17 @@ def get_price(unit, duration_minutes)
   info["#{unit}s_cost"] = tariff[unit] * units_booked
   info[:duration_left] -= minutes_conversion[unit] * units_booked
 
-  # This deals with cases where it's cheaper to use a longer time period (e.g. 1 day = £60) than many smaller time periods (e.g. 2 hours (£44) + 20 minutes (£40) = £84)
+  # Sometimes it's cheaper to use a longer time period (e.g. 1 day = £60) than many smaller time periods (e.g. 2 hours (£44) + 20 minutes (£40) = £84)
   # We need to incrementally test units against larger units e.g. if an hour is cheaper than many minutes, if a day is cheaper than many hours + minutes etc.
-  # smaller_units holds information on what to incrementally test, larger_units contains the units we've already iterated through (starting with week), attempting to see if the sum of smaller units outweights a larger unit, meaning we use the larger (therefore cheaper) unit instead
+  # smaller_units hold information on what to incrementally test, larger_units contains the units we've already iterated through (starting with week), attempting to see if the sum of smaller units outweights a larger unit, meaning we use the larger (therefore cheaper) unit instead
+  # If we enter the if statement, because get_price iterates from largest to smallest, by default we've covered the duration of smaller units, therefore we set duration left to 0 to make sure we don't execute further iterations
   smaller_units = [unit]
   info[:larger_units].each do |larger_unit|
     cost_of_smaller_units = smaller_units.map { |smaller_unit| info["#{smaller_unit}s_cost"] }.inject(:+)
     if cost_of_smaller_units > tariff[larger_unit]
       smaller_units.each { |smaller_unit| info["#{smaller_unit}s_cost"] = 0 }
       info["#{larger_unit}s_cost"] += tariff[larger_unit]
+      # finding that a larger unit is cheaper doesn't preclude the next largest unit being even cheaper, so we need to reset smaller_units
       smaller_units = [larger_unit]
       info[:duration_left] = 0
     else
@@ -28,15 +29,11 @@ def get_price(unit, duration_minutes)
   info[:larger_units].unshift(unit)
 
   if unit == 'minute'
-    info["weeks_cost"] + info["days_cost"] + info["hours_cost"] + info["minutes_cost"]
-    # make sure this isn't explicitly typed, inferred from something
+    tariff.map { |k, _| info["#{k}s_cost"] }.inject(:+)
   else
     info
   end
 end
-
-# better variable names
-# add comments
 
 def tariff
   {
