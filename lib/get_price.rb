20167dfe5acd5078dcedd24e4fcbffac0f86a7c6
Alex Marriott <marriottaog@gmail.com>
2021-09-27 09:42:53 +0100

    change to comments structure:

--- lib/get_price.rb
+++ lib/get_price.rb
@@ -1,34 +1,33 @@
-def get_price(unit, duration_minutes)
-  info = if next_unit_of_time[unit]
-           get_price(next_unit_of_time[unit], duration_minutes)
+def get_price(time_unit, duration_minutes)
+  info = if next_unit_of_time[time_unit]
+           get_price(next_unit_of_time[time_unit], duration_minutes)
          else
            init_info(duration_minutes)
          end
-  # e.g. An hour fits 2 times into 150 minutes, so we would add this to hours_cost (£22 * 2), and take away the quantity of time (60 minutes * 2) from duration_left
-  units_booked = info[:duration_left] / minutes_conversion[unit]
-  info["#{unit}s_cost"] = tariff[unit] * units_booked
-  info[:duration_left] -= minutes_conversion[unit] * units_booked
+
+  # This handles the calculation for adding the cost of a period of time (e.g. 2 days of booking), and subsequently taking away the relevant amount of time from the total.
+  time_units_booked = info[:duration_left] / minutes_conversion[time_unit]
+  info["#{time_unit}s_cost"] = tariff[time_unit] * time_units_booked
+  info[:duration_left] -= minutes_conversion[time_unit] * time_units_booked
 
   # Sometimes it's cheaper to use a longer time period (e.g. 1 day = £60) than many smaller time periods (e.g. 2 hours (£44) + 20 minutes (£40) = £84)
-  # We need to incrementally test units against larger units e.g. if an hour is cheaper than many minutes, if a day is cheaper than many hours + minutes etc.
-  # smaller_units hold information on what to incrementally test, larger_units contains the units we've already iterated through (starting with week), attempting to see if the sum of smaller units outweights a larger unit, meaning we use the larger (therefore cheaper) unit instead
-  # If we enter the if statement, because get_price iterates from largest to smallest, by default we've covered the duration of smaller units, therefore we set duration left to 0 to make sure we don't execute further iterations
-  smaller_units = [unit]
-  info[:larger_units].each do |larger_unit|
-    cost_of_smaller_units = smaller_units.map { |smaller_unit| info["#{smaller_unit}s_cost"] }.inject(:+)
-    if cost_of_smaller_units > tariff[larger_unit]
-      smaller_units.each { |smaller_unit| info["#{smaller_unit}s_cost"] = 0 }
-      info["#{larger_unit}s_cost"] += tariff[larger_unit]
-      # finding that a larger unit is cheaper doesn't preclude the next largest unit being even cheaper, so we need to reset smaller_units
-      smaller_units = [larger_unit]
+  # We need to incrementally test smaller units against larger units, seeing if the sum of smaller units outweights a larger unit e.g. if an hour is cheaper than many minutes, if a day is cheaper than many hours + minutes etc.
+  # smaller_time_units holds information on what to incrementally test, larger_time_units contains the units we've already iterated through (starting with week)
+  smaller_time_units = [time_unit]
+  info[:larger_time_units].each do |larger_time_unit|
+    cost_of_smaller_time_units = smaller_time_units.map { |smaller_time_unit| info["#{smaller_time_unit}s_cost"] }.inject(:+)
+    if cost_of_smaller_time_units > tariff[larger_time_unit]
+      smaller_time_units.each { |smaller_time_unit| info["#{smaller_time_unit}s_cost"] = 0 }
+      info["#{larger_time_unit}s_cost"] += tariff[larger_time_unit]
+      smaller_time_units = [larger_time_unit]
       info[:duration_left] = 0
     else
-      smaller_units.push(larger_unit)
+      smaller_time_units.push(larger_time_unit)
     end
   end
-  info[:larger_units].unshift(unit)
+  info[:larger_time_units].unshift(time_unit)
 
-  if unit == 'minute'
+  if time_unit == 'minute'
     tariff.map { |k, _| info["#{k}s_cost"] }.inject(:+)
   else
     info
@@ -37,6 +36,7 @@ end
 
 # puts get_price('minutes', 253)
 
+# The below methods contain reference information to be passed to the get_price method
 def tariff
   {
     'week' => 105,
@@ -66,7 +66,7 @@ end
 def init_info(duration_minutes)
   {
     duration_left: duration_minutes,
-    larger_units: [],
+    larger_time_units: [],
     'weeks_cost' => 0,
     'days_cost' => 0,
     'hours_cost' => 0,
